790024f698663c87941fb2b438e0e872ac47c468
abdelhadyahmed <a.hadi.1997@gmail.com>
2021-07-13 14:03:54 +0200

    final step in app

--- src/components/manager/Manager.js
+++ src/components/manager/Manager.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+// import NumericInput from 'react-numeric-input'
 import axios from 'axios'
 import useSWR from 'swr'
 import './Manager.css'
@@ -6,6 +7,7 @@ import './Manager.css'
 
 export default function Manager(){
     const [visitors, setVisitors] = useState([]);
+    // const [waitTime, setWaitTime] = useState(0);
     const { data, error } = useSWR('http://127.0.0.1:8000/api/manager', (url) => axios(url).then(res => res.data))
     useEffect(()=>{
         if(data){
@@ -13,18 +15,61 @@ export default function Manager(){
         }
     },[data])
 
-    const deleteVisitor = (id, bool)=>{
+    const deleteVisitor = (currentVisitor, bool)=>{
         if(!bool){
-            alert('رفض')
-        }else{
-            axios.post('http://127.0.0.1:8000/api/deleteVisitor/'+id)
+            axios.post('http://127.0.0.1:8000/api/deleteVisitor/'+currentVisitor.id)
             let updatedVisitors = visitors.filter(visitor =>{
-                return visitor.id !== id;
+                return visitor.id !== currentVisitor.id;
             });
             setVisitors(updatedVisitors)
+            axios.post('http://127.0.0.1:8000/api/command_to_secrtary',{
+                name: currentVisitor.name,
+                command: bool
+            })
+        }else{
+            axios.post('http://127.0.0.1:8000/api/deleteVisitor/'+currentVisitor.id)
+            let updatedVisitors = visitors.filter(visitor =>{
+                return visitor.id !== currentVisitor.id;
+            });
+            setVisitors(updatedVisitors)
+            axios.post('http://127.0.0.1:8000/api/command_to_secrtary',{
+                name: currentVisitor.name,
+                command: bool
+            })
         }
     }
-    
+    // const handleWaitTime = (e)=>{
+    //     setWaitTime(e)
+    // }
+    // const style ={
+    //     wrap: {
+    //         background: '#E2E2E2',
+    //         boxShadow: '0 0 1px 1px #fff inset, 1px 1px 5px -1px #000',
+    //         padding: '2px 2.26ex 2px 2px',
+    //         borderRadius: '6px 3px 3px 6px',
+    //         fontSize: 15
+    //     },
+    //     input: {
+    //         borderRadius: '4px 2px 2px 4px',
+    //         color: 'black',
+    //         padding: '0.1ex 1ex',
+    //         border: '1px solid #ccc',
+    //         marginRight: 4,
+    //         display: 'block',
+    //         fontWeight: 100,
+    //         textShadow: '1px 1px 1px rgba(0, 0, 0, 0.1)'
+    //     },
+    //     'input:focus' : {
+    //         border: '1px inset #69C',
+    //         outline: 'none'
+    //     },
+    //     arrowUp: {
+    //         borderBottomColor: '#435d7d'
+    //     },
+    //     arrowDown: {
+    //         borderTopColor: '#435d7d'
+    //     }
+    // }
     return (
         <div className="container" >
             <div className="table-responsive">
@@ -42,7 +87,7 @@ export default function Manager(){
                                 <th colSpan="2">إسم الزائر</th>
                                 <th>سبب الزيارة</th>
                                 <th>ملاحظات</th>
-                                <th colSpan="2">سماح / رفض</th>
+                                <th colSpan="2">سماح / رفض </th>
                             </tr>
                         </thead>
                         <tbody>
@@ -54,10 +99,9 @@ export default function Manager(){
                                         <td>{visitor.reason}</td>
                                         <td>{visitor.notes}</td>
                                         <td colSpan="2">
-                                            <button onClick={()=>deleteVisitor(visitor.id, 1)}>سماح</button>
-                                            <button onClick={()=>deleteVisitor(visitor.id, 0)}>رفض</button>
-                                            {/* <a  className="edit"><i className="fa fa-check"></i></a>
-                                            <a  className="delete" ><i className="fa fa-times"></i></a> */}
+                                            <button onClick={()=>deleteVisitor(visitor,1)}>سماح</button>
+                                            <button onClick={()=>deleteVisitor(visitor, 0)}>رفض</button>
+                                            {/* <NumericInput onChange={handleWaitTime} value={waitTime} step={1} min={0} max={60} mobile={true} style ={style}/> */}
                                         </td>
                                     </tr>
                                 )
--- src/components/secrtary/Secrtary.js
+++ src/components/secrtary/Secrtary.js
@@ -1,96 +1,118 @@
+import { useState, useEffect } from 'react';
 import axios from 'axios';
-import React, { Component } from 'react'
+import useSWR from 'swr';
 import './Secrtary.css'
 
-export default class Secrtary extends Component {
-    state ={
-        name:"",
-        reason:"",
-        notes: ""
-    }
-    handleSubmit = (e)=>{
+export default function Secrtary(){
+    const [state, setState] = useState({
+        name:'',
+        reason:'',
+        notes:''
+    });
+    const [visitorsCommand, setVisitorsCommand] = useState([]);
+    const { data, error } = useSWR('http://127.0.0.1:8000/api/getVisitorsCommand', (url) => axios(url).then(res => res.data))
+    useEffect(()=>{
+        if(data){
+            setVisitorsCommand(data || [])
+        }
+     },[data])
+    const handleSubmit = (e)=>{
         e.preventDefault();
         if (e.target.name.value === '' || e.target.reason.value ===''){
             return false
         }else{
             axios.post("http://127.0.0.1:8000/api/secretary",{
-                name: this.state.name,
-                reason: this.state.reason,
-                notes: this.state.notes
-            }).then()
-            this.setState({
+                name: state.name,
+                reason: state.reason,
+                notes: state.notes
+            });
+            setState({
                 name: "",
                 reason: "",
                 notes: ""
-            })
+            });
         }
     }
-    handleChange = (e)=>{
-        this.setState({
-            [e.target.id] : e.target.value
+    const handleChange = (e)=>{
+        setState({
+            ...state,
+            [e.target.name] : e.target.value
         })
     }
-    render() {
-        return (
-            <div className="container">
-                <div className="card">
-                    <div className="card-header" >
-                        <h3>  معلومات عن <b>الزائر</b></h3>
-                    </div>
-                    <div className="card-body">
-                        <form onSubmit={this.handleSubmit}>
-                            <div className="row mb-4">
-                                <div className="col">
-                                <div className="form-outline">
-                                    <label className="form-label" htmlFor="form3Example1">إسم الزائر</label>
-                                    <input  type="text" id="name" className="form-control" onChange={this.handleChange} value={this.state.name} />
-                                </div>
-                                </div>
-                                <div className="col">
-                                <div className="form-outline">
-                                    <label className="form-label" htmlFor="form3Example2">سبب الزيارة</label>
-                                    <input type="text" id="reason"  className="form-control"  onChange={this.handleChange} value={this.state.reason} />
-                                </div>
-                                </div>
-                            </div>
+    const deleteVisitor = (id)=>{
+        axios.post('http://127.0.0.1:8000/api/deleteVisitorCommand/'+id)
+            let updatedVisitors = visitorsCommand.filter(visitor =>{
+                return visitor.id !== id;
+            });
+            setVisitorsCommand(updatedVisitors)
+    }
 
-
-                            <div className="form-outline mb-4">
-                                <label className="form-label" htmlFor="form3Example3">ملاحظات</label>
-                                <input type="text" id="notes"  className="form-control"  onChange={this.handleChange} value={this.state.notes} />
-                            </div>
-
-
-                            <div className="card-footer text-center">
-                                <button type="submit" className="btn btn-primary mb-4" >إرسال إلى القائد</button>
-                            </div>
-                        </form>
-                    </div>
+    return (
+        <div className="container">
+            <div className="card">
+                <div className="card-header" >
+                    <h3>  معلومات عن <b>الزائر</b></h3>
                 </div>
-                <div className="card">
-                    <div className="card-header" >
-                        <h1>أوامر الدخول</h1>
-                    </div>
-                    <div className="card-body">
-                        <table className="table table-striped table-hover">
-                            <thead>
-                                <tr>
-                                    <th colSpan="2">إسم الزائر</th>
-                                    <th colSpan="2">الأمر</th>
-                                    <th colSpan="2">مسح طلب الدخول</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td colSpan="2">أحمد محمد</td>
-                                    <td colSpan="2">سماح أو رفض</td>
-                                    <td colSpan="2"><a href="#deleteEmployeeModal" className="delete" data-toggle="modal"><i className="fa fa-times"></i></a></td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
+                <div className="card-body">
+                    <form onSubmit={handleSubmit}>
+                        <div className="row mb-4">
+                            <div className="col">
+                            <div className="form-outline">
+                                <label className="form-label" htmlFor="form3Example1">إسم الزائر</label>
+                                <input  type="text" name="name" className="form-control" onChange={handleChange} value={state.name} />
+                            </div>
+                            </div>
+                            <div className="col">
+                            <div className="form-outline">
+                                <label className="form-label" htmlFor="form3Example2">سبب الزيارة</label>
+                                <input type="text" name="reason"  className="form-control"  onChange={handleChange} value={state.reason} />
+                            </div>
+                            </div>
+                        </div>
+
+
+                        <div className="form-outline mb-4">
+                            <label className="form-label" htmlFor="form3Example3">ملاحظات</label>
+                            <input type="text" name="notes"  className="form-control"  onChange={handleChange} value={state.notes} />
+                        </div>
+
+
+                        <div className="card-footer text-center">
+                            <button type="submit" className="btn btn-primary mb-4" >إرسال إلى القائد</button>
+                        </div>
+                    </form>
                 </div>
             </div>
-        )
-    }
+            <div className="card">
+                <div className="card-header" >
+                    <h1>أوامر الدخول</h1>
+                </div>
+                <div className="card-body">
+                    <table className="table table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <th colSpan="2">إسم الزائر</th>
+                                <th colSpan="2">الأمر</th>
+                                <th colSpan="2">مسح طلب الدخول</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {error ? <tr>content not relod</tr> : null}
+                            {visitorsCommand.map(visitor =>{
+                                return(
+                                    <tr key={visitor.id}>
+                                        <td colSpan="2">{visitor.name}</td>
+                                        <td colSpan="2">{visitor.command ? "سماح": "رفض"}</td>
+                                        <td>
+                                            <button onClick={()=>deleteVisitor(visitor.id)}>مسح الطلب</button>
+                                        </td>
+                                    </tr>
+                                )    
+                            })}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    )
 }
